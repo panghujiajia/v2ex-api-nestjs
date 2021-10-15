7e3a91be750fb54f1f57240d0839bcf1fc7da25f
panghujiajia <qiaoziqiangand@163.com>
2021-10-15 14:31:43 +0800

    增加响应日志

--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -1,25 +1,30 @@
 import {
-	CallHandler,
-	ExecutionContext,
-	Injectable,
-	NestInterceptor,
+    CallHandler,
+    ExecutionContext,
+    Injectable,
+    NestInterceptor
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export interface Response<T> {
-	data: T;
+    data: T;
 }
 
 // 包装response返回
 @Injectable()
 export class TransformInterceptor<T>
-	implements NestInterceptor<T, Response<T>>
+    implements NestInterceptor<T, Response<T>>
 {
-	intercept(
-		context: ExecutionContext,
-		next: CallHandler<T>
-	): Observable<Response<T>> {
-		return next.handle().pipe(map(data => ({ data, status: 200 })));
-	}
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler<T>
+    ): Observable<Response<T>> {
+        return next.handle().pipe(
+            map(data => {
+                console.log(`请求结果：${JSON.stringify(data)}`);
+                return { data, status: 200 };
+            })
+        );
+    }
 }
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -2,18 +2,18 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    const { method, path, body, query, params } = req;
-    console.log(`接口地址：${method} ${path}`);
-    if (JSON.stringify(body) !== '{}') {
-      console.log(`body参数：${JSON.stringify(body)}`);
+    use(req: any, res: any, next: () => void) {
+        const { method, path, body, query, params } = req;
+        console.log(`接口地址：${method} ${path}`);
+        if (JSON.stringify(body) !== '{}') {
+            console.log(`body参数：${JSON.stringify(body)}`);
+        }
+        if (JSON.stringify(query) !== '{}') {
+            console.log(`query参数：${JSON.stringify(query)}`);
+        }
+        if (JSON.stringify(params) !== '{}') {
+            console.log(`params参数：${JSON.stringify(params)}`);
+        }
+        next();
     }
-    if (JSON.stringify(query) !== '{}') {
-      console.log(`query参数：${JSON.stringify(query)}`);
-    }
-    if (JSON.stringify(params) !== '{}') {
-      console.log(`params参数：${JSON.stringify(params)}`);
-    }
-    next();
-  }
 }
